50490f9f5f9e7fddd41948bd20b123b370ea1afc
Srinivas Dadi <srinivasdadi@schemaxtech.com>
2017-12-28 17:17:59 +0530

    qwerty

--- Sample_Charts/ViewController.swift
+++ Sample_Charts/ViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import Charts
+  class ViewController: UIViewController {
+ 
+    @IBOutlet weak var barChartView: BarChartView!
+     var days: [String]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        barChartView.noDataText=" no ma "
+        days = ["monday","tuesday","wed","thur","fri","sat","sun"]
+        
+        let tasks : [Double]  = [1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0]
+        
+         setChart(datePoints: days, values: tasks)
+        
     }
-
+    func setChart(datePoints: [String], values: [Double])  {
+        
+        var dataEntries: [BarChartDataEntry] = []
+        
+        var counter = 0.0
+        
+        for x in 0..<datePoints.count {
+            
+            counter += 1.0
+            
+            let dataentry = BarChartDataEntry(x: values[x],y: counter)
+            
+            dataEntries.append(dataentry)
+            
+        }
+        
+        let chartdataset = BarChartDataSet(values: dataEntries , label: "Days")
+        
+        let chartData = BarChartData()
+        
+        chartData.addDataSet(chartdataset)
+        
+        barChartView.data = chartData
+        
+         chartdataset.colors = ChartColorTemplates.colorful()
+        
+         barChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
+        
+        //barChartView.animate(xAxisDuration: 2.0)
+        
+        
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
